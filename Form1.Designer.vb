8248744860f12eb456ba06cc2bf7d4b945ef7ef8
Ozan Sayılır <ozem1414@hotmail.com>
2020-01-26 12:31:04 -0500

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Start = New System.Windows.Forms.Button()
+        Me.Stp = New System.Windows.Forms.Button()
+        Me.Speed = New System.Windows.Forms.Button()
+        Me.Angle = New System.Windows.Forms.Button()
+        Me.SpeedBox = New System.Windows.Forms.TextBox()
+        Me.AngleBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(42, 50)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(75, 23)
+        Me.Start.TabIndex = 0
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'Stp
+        '
+        Me.Stp.Location = New System.Drawing.Point(42, 79)
+        Me.Stp.Name = "Stp"
+        Me.Stp.Size = New System.Drawing.Size(75, 23)
+        Me.Stp.TabIndex = 1
+        Me.Stp.Text = "Stop"
+        Me.Stp.UseVisualStyleBackColor = True
+        '
+        'Speed
+        '
+        Me.Speed.Location = New System.Drawing.Point(42, 108)
+        Me.Speed.Name = "Speed"
+        Me.Speed.Size = New System.Drawing.Size(75, 23)
+        Me.Speed.TabIndex = 2
+        Me.Speed.Text = "Speed"
+        Me.Speed.UseVisualStyleBackColor = True
+        '
+        'Angle
+        '
+        Me.Angle.Location = New System.Drawing.Point(42, 137)
+        Me.Angle.Name = "Angle"
+        Me.Angle.Size = New System.Drawing.Size(75, 23)
+        Me.Angle.TabIndex = 3
+        Me.Angle.Text = "Angle"
+        Me.Angle.UseVisualStyleBackColor = True
+        '
+        'SpeedBox
+        '
+        Me.SpeedBox.Location = New System.Drawing.Point(123, 109)
+        Me.SpeedBox.Name = "SpeedBox"
+        Me.SpeedBox.Size = New System.Drawing.Size(100, 22)
+        Me.SpeedBox.TabIndex = 4
+        '
+        'AngleBox
+        '
+        Me.AngleBox.Location = New System.Drawing.Point(123, 138)
+        Me.AngleBox.Name = "AngleBox"
+        Me.AngleBox.Size = New System.Drawing.Size(100, 22)
+        Me.AngleBox.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(873, 565)
+        Me.Controls.Add(Me.AngleBox)
+        Me.Controls.Add(Me.SpeedBox)
+        Me.Controls.Add(Me.Angle)
+        Me.Controls.Add(Me.Speed)
+        Me.Controls.Add(Me.Stp)
+        Me.Controls.Add(Me.Start)
+        Me.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Stp As Button
+    Friend WithEvents Speed As Button
+    Friend WithEvents Angle As Button
+    Protected Friend WithEvents Start As Button
+    Friend WithEvents SpeedBox As TextBox
+    Friend WithEvents AngleBox As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,190 @@
+﻿Public Class Form1
+
+
+    Dim IntSpeed As Double 'Speed entered
+    Dim xDelta As Double 'Speed
+    Dim yDelta As Double ' '' 
+    Dim AngleO As Double ' Angle
+
+    Dim OriginalHeight As Double 'Relative position
+    Dim OriginalWidth As Double ' Relative position
+
+    Dim NewX As Int32 'New x for ball
+    Dim NewY As Int32 ' New y for ball
+
+    Dim Xc As Int32 'Ball center coordinates
+    Dim Xy As Int32 ' '' 
+    Dim iXSize As Int32 'Client Dimension
+    Dim iYSize As Int32
+
+    'Rect variables
+    Dim RectBall As Rectangle ' Ball itself
+    Dim myGraphics As Graphics ' Graph object for drawing
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'This is where motion happens
+        If RectBall.Y < 10 Then '10 is used for buffer so it looks neat
+            RectBall.Y = 10
+            yDelta = -yDelta
+
+
+
+        End If
+
+        If RectBall.Y > Me.ClientRectangle.Height - RectBall.Height - 10 Then
+            RectBall.Y = Me.ClientRectangle.Height - RectBall.Height - 10
+            yDelta = -yDelta
+
+        End If
+
+        If RectBall.X < 10 Then
+            RectBall.X = 10
+            xDelta = -xDelta
+
+        End If
+
+        If RectBall.X > Me.ClientRectangle.Width - RectBall.Width - 10 Then
+            RectBall.X = Me.ClientRectangle.Width - RectBall.Width - 10
+            xDelta = -xDelta
+
+        End If
+
+        'Speed is determined now is added to the x , y
+
+        NewX += xDelta
+        NewY += yDelta
+        Invalidate() 'Paint is called at the end after new position is calculated
+
+    End Sub
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+        If xDelta = 0 Then
+            xDelta = 10
+        End If
+        If yDelta = 0 Then
+            yDelta = 10
+        End If
+        'Default speed can be changed 
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Ball_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Speed_Click(sender As Object, e As EventArgs) Handles Speed.Click
+        IntSpeed = Convert.ToDouble(SpeedBox.Text)
+        xDelta = IntSpeed * Math.Cos(AngleO) 'Getting speed from the user
+        yDelta = IntSpeed * Math.Sin(AngleO) 'if no angle is selected zero will be the angle
+        'Motion will happen in one dimension for zero
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DrawBall()
+        'Ball is created at load
+    End Sub
+
+    Private Sub Stp_Click(sender As Object, e As EventArgs) Handles Stp.Click
+        Timer1.Enabled = False
+        'Stops
+
+
+    End Sub
+
+    Private Sub Angle_Click(sender As Object, e As EventArgs) Handles Angle.Click
+        AngleO = Convert.ToDouble(AngleBox.Text) 'Angle is applied to the speed
+        xDelta = xDelta * Math.Cos(AngleO)
+        yDelta = yDelta * Math.Sin(AngleO)
+    End Sub
+
+    Private Sub AngleBox_TextChanged(sender As Object, e As EventArgs) Handles AngleBox.TextChanged
+
+    End Sub
+
+    Public Sub DrawBall()
+        RectBall = New Rectangle()
+        myGraphics = Me.CreateGraphics
+        Dim myPen As New Pen(Drawing.Color.Blue, 5)
+        Dim blueBrush As New SolidBrush(Color.Blue)
+        'Ball is created here
+        'Paint is called when repaint is nessecery
+        'For handling the animation
+
+        RectBall.X = 0
+        RectBall.Y = 0
+        NewX = 200
+        NewY = 350
+        RectBall.Width = 24
+        RectBall.Height = 24
+        iXSize = 24
+        iYSize = 24
+        Xc = (RectBall.X + RectBall.Width) / 2
+        Xy = (RectBall.Y + RectBall.Height) / 2
+        myGraphics.FillRectangle(blueBrush, RectBall)
+        myGraphics.DrawEllipse(myPen, RectBall)
+    End Sub
+
+
+
+    Private Sub Form1_Paint(sender As System.Object, e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
+
+        Dim Pen As New Pen(Me.BackColor, 1)
+        Dim Brush As New SolidBrush(Me.BackColor)
+
+
+        e.Graphics.FillRectangle(Brush, RectBall)
+        e.Graphics.DrawEllipse(Pen, RectBall)
+        'Backcolor ellipse in the old position 
+        Dim myPen As New Pen(Drawing.Color.Blue, 5)
+        Dim blueBrush As New SolidBrush(Color.Blue)
+
+        RectBall.X = NewX 'New position
+        RectBall.Y = NewY
+        Xc = Me.ClientRectangle.Width
+        Xy = Me.ClientRectangle.Height
+        e.Graphics.FillRectangle(blueBrush, RectBall)
+        e.Graphics.DrawEllipse(myPen, RectBall)
+        'Here ball is drawn again with each interval
+        OriginalHeight = RectBall.Y / Me.ClientRectangle.Height 'Used in resize
+        OriginalWidth = RectBall.X / Me.ClientRectangle.Width 'Original position
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        'Resize control
+
+        'Original position relative to window mulitplied with new height and width
+        NewX = OriginalWidth * Me.ClientRectangle.Width
+        NewY = OriginalHeight * Me.ClientRectangle.Height
+
+        If NewY < 10 Then 'Checking limits
+            NewY = 10
+        End If
+
+        If NewY > Me.ClientRectangle.Height - RectBall.Height - 10 Then
+            NewY = Me.ClientRectangle.Height - RectBall.Height - 10
+        End If
+
+        If NewX < 10 Then
+            NewX = 10
+        End If
+
+        If NewX > Me.ClientRectangle.Width - RectBall.Width - 10 Then
+            NewX = Me.ClientRectangle.Width - RectBall.Width - 10
+
+        End If
+
+        OriginalHeight = RectBall.Y / Me.ClientRectangle.Height 'Ratio is calculated again
+        OriginalWidth = RectBall.X / Me.ClientRectangle.Width
+
+        Me.Refresh() 'Paint is called
+
+
+
+    End Sub
+End Class
